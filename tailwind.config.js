5f399b6f0964a20409d6da831a786413ba80653f
Hidecatcat <eric@tryand.co>
2026-03-23 16:22:25 +0800

    ♻️ 重構: 簡化 tailwind.config.js 的內容配置

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './app/components/**/*.{vue,js,ts}',
-    './app/layouts/**/*.vue',
-    './app/pages/**/*.vue',
-    './app/composables/**/*.{js,ts}',
-    './app/plugins/**/*.{js,ts}',
-    './app/app.vue',
-    './app/error.vue',
-  ],
+  content: ['./app/components/**/*.{vue,js,ts}', './app/layouts/**/*.vue', './app/pages/**/*.vue', './app/composables/**/*.{js,ts}', './app/plugins/**/*.{js,ts}', './app/app.vue', './app/error.vue'],
   theme: {
     extend: {
       fontFamily: {
@@ -30,5 +22,4 @@ export default {
     },
   },
   plugins: [],
-}
-
+};
